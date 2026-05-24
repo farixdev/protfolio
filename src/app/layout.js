8ac087452196d166d92dc26d46fc9a40b8b181f0
farixdev <onlyfarix@gmail.com>
2026-05-24 14:38:15 +0500

    Add icons metadata to layout for improved branding

--- src/app/layout.js
+++ src/app/layout.js
@@ -3,6 +3,9 @@ import "./globals.css";
 export const metadata = {
   title: "Faris | Portfolio",
   description: "Web designer and front-end developer based in Pakistan",
+  icons: {
+    icon: "/icons/logo-header-01.png",
+  },
 };
 
 export default function RootLayout({ children }) {
